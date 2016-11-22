3fc07e93629c333eca9d2c29c5fcaa0c04115a52
baesh3744 <baesh3744@naver.com>
2016-11-22 21:56:33 +0900

    back button
    app quits when click back button

--- RockPaperScissors/app/src/main/java/example/com/rockpaperscissors/MainActivity.java
+++ RockPaperScissors/app/src/main/java/example/com/rockpaperscissors/MainActivity.java
@@ -93,6 +93,7 @@ public class MainActivity extends Activity {
                 }
             });
             playButton.startAnimation(fadeIn);
+            super.onBackPressed();
         }
     }
 }
